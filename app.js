e86e1d810eca4828c0d8cee3bc5727aa3595c934
caesar_chi <caesar_chi@hiiir.com>
2014-04-21 15:29:30 +0800

    update method for redmine

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/redmine', routes.redmine);
+app.all('/redmine', routes.redmine);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
