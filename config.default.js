097b12aeb2b54f34091637f4fec854b04ffd3c84
caesar_chi <caesar_chi@hiiir.com>
2014-04-21 14:31:34 +0800

    update for route and config

---
+++ config.default.js
@@ -0,0 +1,5 @@
+module.exports = {
+  domain: "--your-slack-subdomain--",
+  webhook: "--your-slack-webhook--",
+  apiToken: "--you api token--"
+};
--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,29 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.send({
-    status: 200,
-    message: "it is work"
-  });
-};
+var Slack = require('node-slack');
+var slack = new Slack("hpp-hiiir","VTBCYNneXdoNRZjIH5QPYkFj");
 
-exports.webhook = function(req, res){
+domain = "--your-slack-subdomain--";
+webhookToken = "--your-slack-webhook--";
+
+exports.redmine = function(req, res){
+
   console.log("body");
   console.log(req.body);
   console.log("get")
   console.log(req.query);
-  
+
+  slack = new Slack(webhookToken, domain);
+
+  slack.webhook({
+    channel: "#general",
+    username: "webhookbot",
+    text: "This is posted to #general and comes from a bot named webhookbot."
+  }, function(err, response) {
+    console.log(response);
+  });
+
   res.send({
     status: 200,
     message: "it is webhook"
